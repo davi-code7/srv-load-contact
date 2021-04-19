e41fd9f22efa0638a2543058d826cf1d56df9b6e
Davi Silva <davi.silva@code7.com>
2021-04-19 11:54:42 -0300

    feat(controllers): implements NODE_ENV conditions
    implements NODE_ENV conditions."

--- src/controllers/contact/contact.ts
+++ src/controllers/contact/contact.ts
@@ -25,6 +25,40 @@ export async function getContact(req, res) {
 
         console.log(contact);
 
+        const publish = await snsSqs.publishToTopic(
+          'sns-contact',
+          JSON.stringify(contact),
+          'contact',
+          'snsContact',
+          'arn:aws:sns:us-east-1:742104988707:sns-contact.fifo',
+        );
+
+        console.log(publish);
+      });
+
+      return res.status(200).send({ ok: true });
+    } if (
+      process.env.NODE_ENV === 'staging'
+      || process.env.NODE_ENV === 'production'
+    ) {
+      const queues = await orchybase.getQueues(10, {
+        schedule: {
+          $and: {
+            $lt: new Date(),
+          },
+        },
+        state: 'pending',
+      });
+
+      queues.forEach(async (queue) => {
+        const contact = await orchybase.getContact({
+          // @ts-ignore
+          id_load: queue.load.id_load,
+          state: 'pending',
+        });
+
+        console.log(contact);
+
         await orchybase.updateContact(
           { id_contact: BigInt(contact.id_contact) },
           { state: 'working' },
